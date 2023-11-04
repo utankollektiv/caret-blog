438e7d17e13fb99f8fda5f2e84fe5247ea0221bf
David Malmström <syntex@outlook.com>
2023-11-04 15:22:10 +0100

    fix: remove old import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import netlify from "@astrojs/netlify/functions";
 
 import vercel from "@astrojs/vercel/serverless";
 
