71733a4e146b3a44fbb66bfbcab449992f8cf05e
David Malmström <syntex@outlook.com>
2023-11-04 15:05:52 +0100

    feat: add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
   image: {
     service: {
-      entrypoint: "astro/assets/services/noop",
-    },
+      entrypoint: "astro/assets/services/noop"
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
