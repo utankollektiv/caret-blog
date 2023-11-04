1b724db36a3f5e92dfbf688201bc26db6b101d09
David Malmström <syntex@outlook.com>
2023-11-04 15:13:32 +0100

    build: switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
   image: {
     service: {
-      entrypoint: "astro/assets/services/noop"
-    }
+      entrypoint: "astro/assets/services/noop",
+    },
   },
   output: "server",
-  adapter: netlify()
+  adapter: vercel(),
 });
